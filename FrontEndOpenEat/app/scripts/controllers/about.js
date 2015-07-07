1baa321807bb86ae085387802b6c98754f0c4e5e
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-07-07 14:18:25 +0200

    Génération frontend

---
+++ FrontEndOpenEat/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontEndOpenEatApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the frontEndOpenEatApp
+ */
+angular.module('frontEndOpenEatApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ FrontEndOpenEat/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontEndOpenEatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the frontEndOpenEatApp
+ */
+angular.module('frontEndOpenEatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
