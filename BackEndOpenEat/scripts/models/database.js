4f47e6e79bdb9a6c0522f3c991243530caa02f3f
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-07-10 12:35:15 +0200

    Commentaire et remontage pour mieux s'en sortir ^

--- BackEndOpenEat/scripts/models/database.js
+++ BackEndOpenEat/scripts/models/database.js
@@ -2,7 +2,7 @@
  * Created by Guillaume on 08/07/2015.
  */
 
-var pg = require('./dbApi'); // Inclusion de notre api database
+var database = require('./dbApi'); // Inclusion de notre api database
 var fs = require('fs'); // Inclusion de file stream.
 
 /**
@@ -14,12 +14,12 @@ var fs = require('fs'); // Inclusion de file stream.
 exports.initialisationBDD = function() {
 
   // Lecture du fichier contenant la BDD de base.
-  var fileBDD = fs.readFileSync(__dirname + '/init_Database.sql').toString();
+  var sqlInitRequest = fs.readFileSync(__dirname + '/init_Database.sql').toString();
 
   // Lecture du fichier contenant les infos de connexion au format JSON.
   var connectionJSON = fs.readFileSync(__dirname + '/connection_infos.json');
 
   // Connexion pour initialiser la bdd.
-  pg.connect(connectionJSON,fileBDD);
+  database.connect(connectionJSON,sqlInitRequest);
 
 };
--- BackEndOpenEat/scripts/models/dbApi.js
+++ BackEndOpenEat/scripts/models/dbApi.js
@@ -3,14 +3,23 @@
  * Ce module permet de faire des requêtes vers la BDD.
  */
 
-// Extend de postgresql
+// Module PostgreSql
 var pg = require('pg');
 
+/**
+ * Permet la connexion à une Base de Donnée.
+ * @param infosConnectionJSON Les informations de connexion à la BDD au format JSON.
+ * @param sqlRequest La requête SQL à effctuer.
+ */
 exports.connect = function (infosConnectionJSON, sqlRequest) {
+
   pg.connect(infosConnectionJSON, function (err, client, done){
+
+    // Si on arrive pas à se connecter
     if(err){
       return console.log('error fetching client from pool', err);
     }
+    // Si on a réussi alors on reçoit l'objet client
     client.query(sqlRequest, function(err, result){
       done();
       if(err){
