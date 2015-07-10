563cd794cb2558989c6c553e516123c7846f0c31
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-07-10 15:03:28 +0200

    Déclaration d'un service android pour pouvoir échanger entre la montre et le smartphone

---
+++ AndroidOpenEat/mobile/src/main/java/com/opengroup/androidopeneat/WearService.java
@@ -0,0 +1,9 @@
+package com.opengroup.androidopeneat;
+
+import com.google.android.gms.wearable.WearableListenerService;
+
+/**
+ * Created by Guillaume on 10/07/15.
+ */
+public class WearService extends WearableListenerService {
+}
