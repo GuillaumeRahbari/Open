40fca90320c4426817e79839b18a0851a4ce1f49
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-07-20 09:56:26 +0200

    Ajout du fichier contenant les constantes

---
+++ FrontEndOpenEat/app/scripts/services/constants.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name frontEndOpenEatApp.constants
+ * @description
+ * # constants
+ * Constant in the frontEndOpenEatApp.
+ */
+angular.module('frontEndOpenEatApp')
+  .constant('constants', {
+    backendUrl : 'http://localhost:3000/'
+  });
