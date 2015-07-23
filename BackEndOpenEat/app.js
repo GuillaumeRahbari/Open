0e372eb870d674e59e42401b7cfbe609ab004606
unknown <GRA14011@PAR1DSK0361.teamlog.intra>
2015-07-23 12:15:32 +0200

    autorisation de faire des requêtes depuis n'importe quel site

--- BackEndOpenEat/app.js
+++ BackEndOpenEat/app.js
@@ -10,6 +10,13 @@ app.use(logger('dev'));
 
 database.initialisationBDD();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
+  next();
+});
+
 app.use('/',router);
 
 var server = app.listen(3000, function () {
