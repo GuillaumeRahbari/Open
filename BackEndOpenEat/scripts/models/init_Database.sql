268692e8e341e70333d1ffdcb7a830e2807d173d
unknown <GRA14011@PAR1DSK0361.teamlog.intra>
2015-07-23 14:33:20 +0200

    Changement de type de données pour la bdd

--- BackEndOpenEat/scripts/models/init_Database.sql
+++ BackEndOpenEat/scripts/models/init_Database.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "public"."shops" (
     "id" serial,
-    "latitute" real,
-    "longitute" real,
+    "latitude" numeric,
+    "longitude" numeric,
     "description" text,
     PRIMARY KEY ("id")
 );
