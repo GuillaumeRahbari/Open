e4d054b3a5e3a9c9c70e9c93b28703985d18a8c9
unknown <GRA14011@PAR1DSK0361.teamlog.intra>
2015-07-28 10:52:43 +0200

    Changement des tables pour inclure l'utilisateur

--- BackEndOpenEat/scripts/models/init_Database.sql
+++ BackEndOpenEat/scripts/models/init_Database.sql
@@ -1,7 +1,14 @@
+CREATE TABLE IF NOT EXISTS "public"."users" (
+    "id" serial,
+    PRIMARY KEY ("id")
+);
+
 CREATE TABLE IF NOT EXISTS "public"."shops" (
     "id" serial,
     "latitude" numeric,
     "longitude" numeric,
     "description" text,
+    "id_user" serial,
+    FOREIGN KEY ("id_user") REFERENCES users(id),
     PRIMARY KEY ("id")
 );
