1941ab056bf79e0aaad7159fc1f868d1348e11b7
unknown <GRA14011@PAR1DSK0361.teamlog.intra>
2015-07-30 10:14:29 +0200

    Ajout du service constants

---
+++ IonicOpenEat/www/js/services/constants.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name starter.constant:constants
+ * @description
+ * # constants
+ * Constant in the frontEndOpenEatApp.
+ */
+angular.module('starter')
+    .constant('constants', {
+        backendUrl : 'http://localhost:3000/',
+        markerBlue : '/images/marker-blue.png'
+    });
