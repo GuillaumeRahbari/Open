03cd4b266b8474092c26fb05de942afd7d0d3292
unknown <GRA14011@PAR1DSK0361.teamlog.intra>
2015-08-17 15:42:01 +0200

    Ajout de la partie permettant de faire l'appli sur la montre connectée

---
+++ WearOpenEat/app/src/main/java/com/open/openeat/wearopeneat/MainActivity.java
@@ -0,0 +1,112 @@
+package com.open.openeat.wearopeneat;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.google.android.gms.location.LocationServices;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.wearable.view.DismissOverlayView;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.FrameLayout;
+
+/**
+ * Sample that shows how to set up a basic Google Map on Android Wear.
+ */
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnMapLongClickListener {
+
+    private static final LatLng SYDNEY = new LatLng(-33.85704, 151.21522);
+    private Location mLastLocation;
+
+    /**
+     * Overlay that shows a short help text when first launched. It also provides an option to
+     * exit the app.
+     */
+    private DismissOverlayView mDismissOverlay;
+
+    /**
+     * The map. It is initialized when the map has been fully loaded and is ready to be used.
+     *
+     * @see #onMapReady(com.google.android.gms.maps.GoogleMap)
+     */
+    private GoogleMap mMap;
+
+    public void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+
+        // Set the layout. It only contains a SupportMapFragment and a DismissOverlay.
+        setContentView(R.layout.activity_main);
+
+        // Retrieve the containers for the root of the layout and the map. Margins will need to be
+        // set on them to account for the system window insets.
+        final FrameLayout topFrameLayout = (FrameLayout) findViewById(R.id.root_container);
+        final FrameLayout mapFrameLayout = (FrameLayout) findViewById(R.id.map_container);
+
+        // Set the system view insets on the containers when they become available.
+        topFrameLayout.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+            @Override
+            public WindowInsets onApplyWindowInsets(View v, WindowInsets insets) {
+                // Call through to super implementation and apply insets
+                insets = topFrameLayout.onApplyWindowInsets(insets);
+
+                FrameLayout.LayoutParams params =
+                        (FrameLayout.LayoutParams) mapFrameLayout.getLayoutParams();
+
+                // Add Wearable insets to FrameLayout container holding map as margins
+                params.setMargins(
+                        insets.getSystemWindowInsetLeft(),
+                        insets.getSystemWindowInsetTop(),
+                        insets.getSystemWindowInsetRight(),
+                        insets.getSystemWindowInsetBottom());
+                mapFrameLayout.setLayoutParams(params);
+
+                return insets;
+            }
+        });
+
+        // Obtain the DismissOverlayView and display the intro help text.
+        mDismissOverlay = (DismissOverlayView) findViewById(R.id.dismiss_overlay);
+        mDismissOverlay.setIntroText(R.string.intro_text);
+        mDismissOverlay.showIntroIfNecessary();
+
+        // Obtain the MapFragment and set the async listener to be notified when the map is ready.
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Map is ready to be used.
+        mMap = googleMap;
+
+        // Set the long click listener as a way to exit the map.
+        mMap.setOnMapLongClickListener(this);
+
+        // Add a marker with a title that is shown in its info window.
+        mMap.addMarker(new MarkerOptions().position(SYDNEY)
+                .title("Sydney Opera House"));
+
+        // Move the camera to show the marker.
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, 10));
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        // Display the dismiss overlay with a button to exit this activity.
+        mDismissOverlay.show();
+    }
+}
