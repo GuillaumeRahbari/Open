f4f729ed123bf857227706d0ba40bf41aaeb1d6a
unknown <GRA14011@PAR1DSK0361.teamlog.intra>
2015-08-31 10:06:02 +0200

    Modification de l'ip^du serveur

--- IonicOpenEat/www/js/services/constants.js
+++ IonicOpenEat/www/js/services/constants.js
@@ -9,7 +9,7 @@
  */
 angular.module('starter')
     .constant('constants', {
-        backendUrl: 'http://10.170.2.167:3000/', // dev
+        backendUrl: 'http://10.170.2.174:3000/', // dev
         googleMap : {
             ios : {
                 scheme: 'comgooglemaps://',
